a8faa0124af070f835a058311f74bd7dd1c5c395
Tirtha Subedi <31990410+tirthasubedi@users.noreply.github.com>
2018-08-03 18:49:50 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+###############################################################################################################################
+#Creator: Tirtha Subedi
+#Purpose: Game to 
+
+from game import *
+
+def main():
+    global root
+    gm = Game()
+
+main()
